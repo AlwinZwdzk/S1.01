8927d3b400d7064eb2c21adc82610dc7e736bbb0
AlwinZwdzk <124166176+AlwinZwdzk@users.noreply.github.com>
2024-01-16 13:03:20 +0100

    Update tr.java

--- tr.java
+++ tr.java
@@ -8,3 +8,26 @@
                     }
                 nav.println("\t\t</tr>");
             }
+
+
+
+void inittab(){
+        for(int i = 0; i < nbLigne; i++){
+            for(int j = 0; j < nbColonne; j++){
+                tableur[i][j]= "";
+            }
+        }
+        for(int i = 0; i < nbLigne; i++){
+            tableur[i+1][0]=Integer.toString(i+1);
+        }
+        for(int i = 0; i < nbColonne; i++){
+            int div = i/26;
+            if(div>0){
+                for(int j=0;j<div;j++){
+                    tableur[0][i+1]=alphabet[div-1]+alphabet[i%26];
+                }
+            }
+            else tableur[0][i+1]=alphabet[i%26];
+        }
+    }
+    
